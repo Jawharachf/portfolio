69424a9a029b2048fa05e4aa1dcd834d5f5241e2
Jawharachf <jawhara.chafi@hotmail.fr>
2024-10-27 19:06:08 +0100

    Mode clair par défaut

--- script_js.js
+++ script_js.js
@@ -2,19 +2,19 @@ document.addEventListener("DOMContentLoaded", function() {
     const toggleButton = document.getElementById('toggle-theme');
   
     // Vérifie si un thème est déjà enregistré dans le localStorage
-    if (localStorage.getItem('theme') === 'light') {
-      document.body.classList.add('light-mode');
+    if (localStorage.getItem('theme') === 'dark') {
+      document.body.classList.add('dark-mode');
       toggleButton.textContent = 'Mode sombre';
     }
   
     // Fonction de basculement
     toggleButton.addEventListener('click', () => {
-      document.body.classList.toggle('light-mode');
+      document.body.classList.toggle('dark-mode');
       
       // Change le texte du bouton
-      if (document.body.classList.contains('light-mode')) {
+      if (document.body.classList.contains('dark-mode')) {
         toggleButton.textContent = 'Mode sombre';
-        localStorage.setItem('theme', 'light');  // Enregistre le choix en mode clair
+        localStorage.setItem('theme', 'dark');  // Enregistre le choix en mode clair
       } else {
         toggleButton.textContent = 'Mode clair';
         localStorage.setItem('theme', 'dark');  // Enregistre le choix en mode sombre
